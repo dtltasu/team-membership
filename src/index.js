2123dd8408f802daefd7ed42a369b6cbbfebb0ee
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2023-08-24 22:02:09 -0300

    Using rest API

--- src/index.js
+++ src/index.js
@@ -4,68 +4,27 @@ import { getOctokit, context } from '@actions/github'
 run()
 
 async function run() {
+  try {
+    const api = getOctokit(getInput("GITHUB_TOKEN", { required: true }), {})
 
-    try {
+    const organization = getInput("organization") || context.repo.owner
+    const username = getInput("username", { required: true })
+    const team = getInput("team", { required: true })
+    let isTeamMember = false;
 
-        const api = getOctokit(getInput("GITHUB_TOKEN", { required: true }), {})
+    console.log(`Will check if ${username}@${organization} belongs to ${team}`)
 
-        const organization = getInput("organization") || context.repo.owner
-        const username = getInput("username")
-        const team = getInput("team")
-
-        console.log(`Getting teams for ${username} in org ${organization}. Will check if belongs to ${team}`)
-
-        const query = `query($cursor: String, $org: String!, $userLogins: [String!], $username: String!)  {
-            user(login: $username) {
-                id
-            }
-            organization(login: $org) {
-              teams (first:100, userLogins: $userLogins, after: $cursor) {
-                  nodes {
-                    name
-                }
-                pageInfo {
-                  hasNextPage
-                  endCursor
-                }
-              }
-            }
-        }`
-
-        let data
-        let teams = []
-        let cursor = null
-
-        // We need to check if the user exists, because if it doesn't exist then all teams in the org
-        // are returned. If user doesn't exist graphql will throw an exception
-        // Paginate
-        do {
-            data = await api.graphql(query, {
-                "cursor": cursor,
-                "org": organization,
-                "userLogins": [username],
-                "username": username
-            })
-
-            teams = teams.concat(data.organization.teams.nodes.map((val) => {
-                return val.name
-            }))
-
-            cursor = data.organization.teams.pageInfo.endCursor
-        } while (data.organization.teams.pageInfo.hasNextPage)
-
-        let isTeamMember = teams.some((teamName) => {
-            return team.toLowerCase() === teamName.toLowerCase()
-        })
-
-        console.log(`teams: [${teams.join(', ')}] for ${username} in org ${organization}`)
-        console.log(`Belongs to ${team}?: ${isTeamMember}`)
-
-        setOutput("teams", teams)
-        setOutput("isTeamMember", isTeamMember)
-
-    } catch (error) {
-        console.log(error)
-        setFailed(error.message)
-    }
+    api.rest
+       .orgs
+       .getMembershipForUserInOrg({ org: organization, team_slug: team, username: username})
+       .then((response) => {
+         isTeamMember = response.body.state === "active"
+         setOutput("isTeamMember", isTeamMember) })
+       .catch((error) => {
+         onsole.log(error)
+         setFailed(error.message) })
+  } catch (error) {
+    console.log(error)
+    setFailed(error.message)
+  }
 }
